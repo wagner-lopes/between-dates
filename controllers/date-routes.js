8cc32ccb2b9ba4968ff1f1fdaa32667ffac9028a
Wagner Lopes <wagnerlopes.bh@gmail.com>
2022-08-21 08:58:20 +0930

    Refactoring to create utils folder with usefulMethods

--- controllers/date-routes.js
+++ controllers/date-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 const { DateTime } = require("luxon");
+const {
+    getWeekdays,
+    applyDateFormat,
+    roundResults
+} = require('../utils/usefulMethods')
 
 router.post('/interval', (req, res) => {
     //Extract parameters from the body of the request
@@ -52,63 +57,5 @@ router.post('/interval', (req, res) => {
 
     res.send(result)
 });
-
-function applyDateFormat(result, format) {
-    let multiplier;
-    switch (format) {
-        case "seconds":
-            multiplier = 24 * 60 * 60;
-            break;
-        case "minutes":
-            multiplier = 24 * 60;
-            break;
-        case "hours":
-            multiplier = 24;
-            break;
-        case "years":
-            return formatInYears(result);
-            break;
-        default:
-            return result;
-    }
-
-    return applyMultiplier(result, format, multiplier);
-}
-
-function formatInYears(result) {
-    result.weekdays = `${result.weekdays / 365} years`;
-    result.completeWeeks = `${result.completeWeeks / (365 / 7)} years`;
-    result.days = `${result.days / 365} years`;
-
-    return result;
-}
-
-function applyMultiplier(result, format, multiplier) {
-    let capitalisedFormat = format.charAt(0).toUpperCase() + format.slice(1);
-    result[`weekdaysIn${capitalisedFormat}`] = `${result.weekdays * multiplier} ${format}`;
-    result[`completeWeeksIn${capitalisedFormat}`] = `${result.completeWeeks * 7 * multiplier} ${format}`;
-    result[`daysIn${capitalisedFormat}`] = `${result.days * multiplier} ${format}`;
-
-    return result;
-}
-
-function roundResults(result) {
-    //Get the integer part of the result
-    result.completeWeeks = Math.floor(result.completeWeeks);
-    result.days = Math.floor(result.days);
-    result.weekdays = Math.floor(result.weekdays);
-
-    return result;
-}
-
-function getWeekdays(startDate, endDate) {
-    let weekdays = 0;
-    while (startDate < endDate) {
-        startDate = startDate.plus({ days: 1 });
-        if (startDate.weekday < 6) weekdays++;
-    }
-
-    return weekdays;
-}
 
 module.exports = router;
